e9f0574a96c1550934b472b4b1b699cad882a56a
Abdulla-Younis <38014249+Abdulla-Younis@users.noreply.github.com>
2019-03-18 12:34:11 +0200

    Add files via upload

---
+++ fisrt_Assigenment.java
@@ -0,0 +1,30 @@
+package automation_course;
+
+import java.util.Scanner;
+
+public class fisrt_Assigenment {
+
+	public static void main(String[] args) {
+		double a, b, res;
+		       Scanner X = new Scanner(System.in);
+		   
+	       System.out.print("Please Enter Two Numbers : ");
+	       a = X.nextDouble();
+	       b = X.nextDouble();
+	   
+		   
+	       res = a + b;
+	       System.out.println("Submission  = " +res);
+		   
+	       res = a - b;
+	       System.out.println("Subtraction = " +res);
+		   
+	       res = a * b;
+	       System.out.println("Multiplication = " +res);
+		   
+	       res = a / b;
+	       System.out.println("Division = " +res);
+
+	}
+
+}
